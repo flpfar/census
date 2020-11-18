19ba3ef4801347f3c4bab0ec4afb046e08561d18
Felipe Rosa <flp.far@gmail.com>
2020-11-18 18:36:45 -0300

    Set up sidekiq/redis

--- lib/census.rb
+++ lib/census.rb
@@ -1,12 +1,13 @@
+require 'bundler'
+Bundler.require(:default)
+
 require_relative './census/version'
 require_relative './census/utils/api'
 require_relative './census/cli'
+require_relative '../config/sidekiq'
 require_relative './census/db/db_initializer'
 require_relative './census/db/db_connection'
 require_relative './census/controllers/menu_controller'
 require_relative './census/controllers/states_controller'
 require_relative './census/controllers/cities_controller'
 require_relative './census/controllers/names_controller'
-
-require 'bundler'
-Bundler.require(:default)
