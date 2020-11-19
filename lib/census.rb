23997c0f8dfacff50b5c8a698d3030bd84d05190
Felipe Rosa <flp.far@gmail.com>
2020-11-18 23:32:54 -0300

    Fix requiring cli before version issue

--- lib/census.rb
+++ lib/census.rb
@@ -1,5 +1,7 @@
 require 'bundler'
 Bundler.require(:default)
 
+require_relative './census/version'
+require_relative './census/cli'
 Dir.glob('config/initializers/*.rb') { |file| require_relative "../#{file}" }
 Dir.glob('lib/census/**/*.rb') { |file| require_relative file.gsub('lib/', './') }
