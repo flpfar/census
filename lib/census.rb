0a266690cd7334ed4f7571c7ff22af512bf94a85
Felipe Rosa <flp.far@gmail.com>
2020-11-19 12:27:27 -0300

    Add method to require files in order

--- lib/census.rb
+++ lib/census.rb
@@ -1,7 +1,34 @@
 require 'bundler'
 Bundler.require(:default)
 
-require_relative './census/version'
-require_relative './census/cli'
-Dir.glob('config/initializers/*.rb') { |file| require_relative "../#{file}" }
-Dir.glob('lib/census/**/*.rb') { |file| require_relative file.gsub('lib/', './') }
+class CensusLoader
+  def initialize
+    @files = []
+    @files.push(*(Dir.glob('config/initializers/*.rb').map { |file| "../#{file}" }))
+    @files.push(*(Dir.glob('lib/census/**/*.rb').map { |file| file.gsub('lib/', './') }))
+    require_all_files
+  end
+
+  private
+
+  attr_accessor :files
+
+  def require_all_files # rubocop:disable Metrics/MethodLength
+    i = 0
+    while i < files.length
+      begin
+        require_relative files[i]
+      rescue NameError
+        i += 1
+      else
+        while i.positive?
+          files.push(files.shift)
+          i -= 1
+        end
+        files.shift
+      end
+    end
+  end
+end
+
+CensusLoader.new
