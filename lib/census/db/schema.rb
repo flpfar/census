226ae6b483a47c904a08b98fd40cc6f34f3ed860
flpfar <flp.far@gmail.com>
2020-11-26 11:28:00 -0300

    Add table rankings in schema

--- lib/census/db/schema.rb
+++ lib/census/db/schema.rb
@@ -12,6 +12,16 @@ CREATE_TABLE_CITIES = 'CREATE TABLE IF NOT EXISTS cities ('\
                         'PRIMARY KEY (id)'\
                       ');'.freeze
 
+CREATE_TABLE_RANKINGS = 'CREATE TABLE IF NOT EXISTS rankings ('\
+                          'id SERIAL PRIMARY KEY,'\
+                          'locale_id integer NOT NULL,'\
+                          'name varchar(40) NOT NULL,'\
+                          'rate integer NOT NULL,'\
+                          'ranking integer NOT NULL,'\
+                          'gender CHAR(1) NOT NULL,'\
+                          'UNIQUE (locale_id, name, gender)'\
+                        ');'.freeze
+
 REMOVE_NOTICE_IF_TABLE_EXISTS = 'SET client_min_messages = error;'.freeze
 
-CREATE_TABLES = (CREATE_TABLE_STATES + CREATE_TABLE_CITIES).freeze
+CREATE_TABLES = (CREATE_TABLE_STATES + CREATE_TABLE_CITIES + CREATE_TABLE_RANKINGS).freeze
