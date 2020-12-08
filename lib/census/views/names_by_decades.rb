4ea196fa3df7d03815c24f755b299a40e6908e33
flpfar <flp.far@gmail.com>
2020-12-08 18:19:52 -0300

    Update view to use NameByDecadeResult

--- lib/census/views/names_by_decades.rb
+++ lib/census/views/names_by_decades.rb
@@ -1,35 +1,35 @@
-class NamesByDecadesView
-  class << self
-    def index
-      print 'Informe o(s) nome(s) desejado(s) (separados por vírgulas): '
-      names = gets.chomp
-      NamesController.search_names(names)
-    end
-
-    def show(names_hash)
-      return puts 'Nome(s) não encontrado(s)' if names_hash.empty? || names_hash[:names].empty?
-
-      puts create_table(names_hash)
-
-      return if names_hash[:names_not_found].empty?
-
-      puts "Nome(s) não encontrado(s): '#{names_hash[:names_not_found].join(', ')}'."
-    end
-
-    private
-
-    def create_table(names_hash) # rubocop:disable Metrics/AbcSize
-      headings = ['']
-      decades = Hash.new { |h, k| h[k] = [] }
-      names_hash[:names].each do |item|
-        headings << item.name
-        item.decades.each { |k, v| decades[k] << v }
+module Views
+  class NamesByDecades
+    class << self
+      def index
+        print 'Informe o(s) nome(s) desejado(s) (separados por vírgulas): '
+        names = gets.chomp
+        NamesController.search_names(names)
       end
 
-      rows = []
-      decades.each { |k, v| rows << v.unshift(k) }
+      def show(names_result)
+        return 'Nome(s) não encontrado(s)' if names_result.nil? || names_result.names.empty?
 
-      Terminal::Table.new headings: headings, rows: rows
+        return create_table(names_result) if names_result.names_not_found.empty?
+
+        "Nome(s) não encontrado(s): '#{names_result.names_not_found.join(', ')}'."
+      end
+
+      private
+
+      def create_table(names_result) # rubocop:disable Metrics/AbcSize
+        headings = ['']
+        decades = Hash.new { |h, k| h[k] = [] }
+        names_result.names.each do |item|
+          headings << item.name
+          item.decades.each { |k, v| decades[k] << v }
+        end
+
+        rows = []
+        decades.each { |k, v| rows << v.unshift(k) }
+
+        Terminal::Table.new headings: headings, rows: rows
+      end
     end
   end
 end
